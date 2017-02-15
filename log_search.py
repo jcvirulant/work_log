2084a6410eabc64df57a398f1e1f0b075eb1918d
ReluctantCapitalist <jeff@virulantconsulting.com>
2017-02-15 11:12:10 -0600

    Changed formatting for the search results; added a 'no results'

--- log_search.py
+++ log_search.py
@@ -12,11 +12,23 @@ class Search:
             self.rff('task_name')
 
     def rff(self, fieldname):  # rff = read from file
+        count = 0
         csvfile = open('work_log.csv', 'r')
         search = input('What {} would you like to search?'.format(fieldname))
         reader = csv.DictReader(csvfile)
         for row in reader:
             if search in row[str(fieldname)]:
-                print(row)
-        if input('Search again? Y/n').lower() == 'y':
+                count += 1
+                print(
+                    '\n\nFirst Name: ' + row['first_name'] + '\n'
+                    'Last Name: ' + row['last_name'] + '\n'
+                    'Task: ' + row['task_name'] + '\n'
+                    'Duration: ' + row['time_spent'] + '\n'
+                    'Notes: ' + row['notes'] + '\n'
+                    'Date: ' + row['date'] + '\n'
+                    )
+                print('_' * 50)
+        if count == 0:
+            print('No results found')
+        if input('\n\nSearch again? Y/n').lower() == 'y':
             Search()
