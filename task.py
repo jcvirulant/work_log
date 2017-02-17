0423af0f146ae62af3b278bfdef7786d9a2e9832
ReluctantCapitalist <jeff@virulantconsulting.com>
2017-02-17 13:49:55 -0600

    Added task class

---
+++ task.py
@@ -0,0 +1,24 @@
+import csv
+
+
+class Task(object):
+    """Task the object for each individual task that is created."""
+
+    def __init__(self):
+        fmt = ('%m-%d-%y')
+    self.task_name = input('What is the task name? ')
+    self.time_spent = input('How mush time was spent? ')
+    if input('Was the task done today?') == 'y':
+        self.date = datetime.date.today()
+        self.date = date.strftime(fmt)
+    else:
+        self.date = input('What is the task date? (MM-DD-YY) ')
+    self.notes = input('Notes (hit enter to skip)? ')
+    self.my_dict = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'task_name': self.task_name,
+            'time_spent': self.time_spent,
+            'notes': self.notes,
+            'date': self.date
+            }
